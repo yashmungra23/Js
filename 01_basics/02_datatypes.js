e82945705109ccfc94c155a94ed3b802893dba8e
yashmungra23 <yashmungra75@gmail.com>
2026-03-23 16:02:56 +0000

    dataTypes in Js

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,14 @@
+"use strict"//treat all JS code as newer version
+
+let name = "yash" //string
+let age = 18 //int // you can use "bigint" for big integers
+let isLoggedin  = true //bool type
+// null => standalone value
+// undefined => 
+// symbol => for uniqueness
+
+
+//object
+
+console.log(typeof null);
+console.log(typeof undefined);
