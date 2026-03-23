b2c43443c1126431c5c4cd510c9a492535031971
yashmungra23 <yashmungra75@gmail.com>
2026-03-23 15:56:43 +0000

    varriables in js

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,1 +1,12 @@
-console.log("yash");
+const accountId = 123
+let accountEmail = "yash@gmail.com"
+var accountPassword = "123"
+accountCity = "Jamnagar"
+ 
+//accountId = 2 // not changable
+
+accountEmail = "ym@gamil.com"
+accountPassword = "245"
+accountCity = "rajkot"
+
+console.table([accountEmail , accountId , accountPassword , accountCity]);
