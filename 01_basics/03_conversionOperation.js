7c1465dd8d0036623a706d884eba6a19c0b04bad
yashmungra23 <yashmungra75@gmail.com>
2026-03-23 16:08:44 +0000

    Conversion operation In js

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,13 @@
+let score = "33";
+
+console.log(typeof score);
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33abc" => NaN
+/*
+    in bool 
+    1 => true
+    2 => false
+*/
