a2c1ac8892fe8d46769f0106a287a86181001423
yashmungra23 <yashmungra75@gmail.com>
2026-03-23 16:49:41 +0000

    Comparisions

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,20 @@
+console.log(2 < 1);
+console.log(2 > 1);
+console.log(2 == 1);
+console.log(2 >=1);
+
+console.log(null > 0); //false
+console.log(null == 0); //false
+console.log(null <= 0); //true
+console.log(null >= 0); //true
+
+//for undefined all operations considered as false
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+console.log(undefined <= 0);
+console.log(undefined > 1); 
+
+// ===
+
+console.log("3" === 3); //false
