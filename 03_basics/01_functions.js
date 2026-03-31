7010680bce16db52cacf0155558690ac04475429
yashmungra23 <yashmungra75@gmail.com>
2026-03-31 05:32:56 +0000

    Function

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -22,3 +22,39 @@ function loggedIn(user){
 
 console.log(loggedIn("yash")); // yash just logged in 
 
+function calaulateCartPrice(...num1){
+    return num1;
+}
+
+console.log(calaulateCartPrice(23,45,45,78,89));
+// [ 23, 45, 45, 78, 89 ]
+
+function calaulateCart(value1, value2, ...num){
+    return num;
+}
+
+console.log(calaulateCart(400,3,2,12)); // [ 2, 12 ]
+
+// object passeed to the function
+
+const user = {
+    name : "JS",
+    price : "14 $"
+}
+
+function handleObject(anyobject){
+    console.log(`Course name is ${anyobject.name} and price is ${anyobject.price}`);
+}
+
+handleObject(user); // Course name is JS and price is 14 $
+
+// array passed to the function
+
+const arr = [45,658,758,78];
+
+function arrelements(getArray){
+    return getArray;
+}
+
+console.log(arr); // [ 45, 658, 758, 78 ]
+
