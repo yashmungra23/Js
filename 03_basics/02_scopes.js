43db27be90d49929fbc369722d8ddcbb68178da6
yashmungra23 <yashmungra75@gmail.com>
2026-04-05 12:29:34 +0000

    Scope and nested Scope

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -12,3 +12,45 @@ if(true){
 console.log(a); //
 // console.log(c); // give an error
 // console.log(d); // give an error
+
+// nested scope
+
+function one(){
+    const userName = "YASH";
+    function two(){
+        const age = 18;
+        console.log(userName);
+    }
+    // console.log(age); // out of scope
+
+    two();
+}
+
+one();
+
+if(true) {
+    const name = "yash";
+    if(true){
+        const age = 18;
+        console.log(name + " Age is :" , age); // return yash Age is 18
+    }
+    // console.log(age); // out of scope
+}
+
+// ======================================================================================= 
+console.log("===========================================");
+
+console.log(addone(10)); // output 11 
+
+function addone(num){
+    return num + 1;
+}
+
+// console.log(addTwo(10)) // gives an error
+
+const addTwo = function(num){
+    return num + 2;
+}
+// this is veriable function
+
+console.log(addTwo(10)); // output 12
